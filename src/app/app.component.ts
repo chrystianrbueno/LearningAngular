ae1e9baea9712140ba134e7ebfa18634e6bc17a9
Chrystian Rocha <chrystianrbueno@gmail.com>
2021-09-02 10:17:39 -0300

    Getting Dataset from api - Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './posts'
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'demoservice';
+
+export class AppComponent implements OnInit{
+  posts: Post [];
+
+  constructor(private postService: PostsService){
+    this.posts = [];
+  }
+
+  ngOnInit(): void {
+    this.postService.buscarTodosPosts().subscribe(
+      dados => this.posts = dados
+    );
+  }
+
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Post } from './posts';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PostsService {
+
+  private urlBase = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(private http: HttpClient) { }
+
+  buscarTodosPosts(): Observable<Post[]>  {  
+    return this.http.get<Post[]>(this.urlBase);
+  }
+
+
+}
